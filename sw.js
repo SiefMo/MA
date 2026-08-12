60c47c714ab2f743676278dbfbe7c5cc7e00997b
sief mohamed <sief2455@gmail.com>
2026-08-12 08:18:32 +0300

    Move website files to root

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-const CACHE='maghraby-v2';
-const A=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./public/logo.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).then(r=>{let z=r.clone();caches.open(CACHE).then(c=>c.put(e.request,z));return r}).catch(()=>caches.match('./index.html'))))});
+const C='maghraby-pro-v3';const A=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./public/logo.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{let y=x.clone();caches.open(C).then(c=>c.put(e.request,y));return x}).catch(()=>caches.match('./index.html'))))});
